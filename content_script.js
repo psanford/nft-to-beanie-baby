d484cd4d9770698d761749f325f9ff5d5a226e3e
Peter Sanford <psanford@sanford.io>
2022-01-18 12:08:32 -0800

    Fix whitespace

--- content_script.js
+++ content_script.js
@@ -3,45 +3,45 @@ walk(document.body);
 
 function walk(node)
 {
-	var child, next;
+  var child, next;
 
-	var tagName = node.tagName ? node.tagName.toLowerCase() : "";
-	if (tagName == 'input' || tagName == 'textarea') {
-		return;
-	}
-	if (node.classList && node.classList.contains('ace_editor')) {
-		return;
-	}
+  var tagName = node.tagName ? node.tagName.toLowerCase() : "";
+  if (tagName == 'input' || tagName == 'textarea') {
+    return;
+  }
+  if (node.classList && node.classList.contains('ace_editor')) {
+    return;
+  }
 
-	switch ( node.nodeType )
-	{
-		case 1:  // Element
-		case 9:  // Document
-		case 11: // Document fragment
-			child = node.firstChild;
-			while ( child )
-			{
-				next = child.nextSibling;
-				walk(child);
-				child = next;
-			}
-			break;
+  switch ( node.nodeType )
+  {
+    case 1:  // Element
+    case 9:  // Document
+    case 11: // Document fragment
+      child = node.firstChild;
+      while ( child )
+      {
+        next = child.nextSibling;
+        walk(child);
+        child = next;
+      }
+      break;
 
-		case 3: // Text node
-			handleText(node);
-			break;
-	}
+    case 3: // Text node
+      handleText(node);
+      break;
+  }
 }
 
 function handleText(textNode)
 {
-	var v = textNode.nodeValue;
+  var v = textNode.nodeValue;
 
-	v = v.replace(/\bAn NFT\b/g, "A Beanie Baby");
-	v = v.replace(/\ban NFT\b/g, "a Beanie Baby");
-	v = v.replace(/\bNFTs\b/g, "Beanie Babies");
-	v = v.replace(/\bNFT\b/g, "Beanie Baby");
-	v = v.replace(/\bnft\b/g, "beanie baby");
+  v = v.replace(/\bAn NFT\b/g, "A Beanie Baby");
+  v = v.replace(/\ban NFT\b/g, "a Beanie Baby");
+  v = v.replace(/\bNFTs\b/g, "Beanie Babies");
+  v = v.replace(/\bNFT\b/g, "Beanie Baby");
+  v = v.replace(/\bnft\b/g, "beanie baby");
   v = v.replace(/\bNon-fungible token\b/g, "Non-fungible baby");
   v = v.replace(/\bNon-Fungible Token\b/g, "Non-Fungible Baby");
   v = v.replace(/\bNon-Fungible token\b/g, "Non-Fungible baby");
@@ -49,5 +49,5 @@ function handleText(textNode)
   v = v.replace(/\bNon-fungible tokens\b/g, "Non-fungible babies");
   v = v.replace(/\bnon-fungible tokens\b/g, "non-fungible babies");
 
-	textNode.nodeValue = v;
+  textNode.nodeValue = v;
 }
